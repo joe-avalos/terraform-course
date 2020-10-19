01594c84022bd0db7377758e641c5bbd9f7b164a
joe-avalos <jose.b.avalos@gmail.com>
2020-10-19 11:01:14 -0500

    master - correct ami images for demo-1 and demo-2

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -11,9 +11,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-0739f8cdb239fe9ae"
+    us-west-2 = "ami-08d0eee5e00da8a9b"
+    eu-west-1 = "ami-09b9e380df60300c8"
   }
 }
 
