626db8fa783dab28bb7e87b5d6a108126c22357e
joe-avalos <jose.b.avalos@gmail.com>
2020-10-19 13:13:43 -0500

    master - demo5 datasources

--- demo-5/securitygroup.tf
+++ demo-5/securitygroup.tf
@@ -1,20 +1,20 @@
-data "aws_ip_ranges" "european_ec2" {
-  regions  = ["eu-west-1", "eu-central-1"]
+data "aws_ip_ranges" "usa_ec2" {
+  regions  = ["us-east-1","us-east-2", "us-west-1", "us-west-2"]
   services = ["ec2"]
 }
 
-resource "aws_security_group" "from_europe" {
-  name = "from_europe"
+resource "aws_security_group" "from_usa" {
+  name = "from_usa"
 
   ingress {
     from_port   = "443"
     to_port     = "443"
     protocol    = "tcp"
-    cidr_blocks = slice(data.aws_ip_ranges.european_ec2.cidr_blocks, 0, 50)
+    cidr_blocks = slice(data.aws_ip_ranges.usa_ec2.cidr_blocks, 0, 50)
   }
   tags = {
-    CreateDate = data.aws_ip_ranges.european_ec2.create_date
-    SyncToken  = data.aws_ip_ranges.european_ec2.sync_token
+    CreateDate = data.aws_ip_ranges.usa_ec2.create_date
+    SyncToken  = data.aws_ip_ranges.usa_ec2.sync_token
   }
 }
 
