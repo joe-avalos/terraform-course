be930d2d93f6c787a7b977fb1d0c04f0efde2e95
joe-avalos <jose.b.avalos@gmail.com>
2020-10-20 19:10:34 -0500

    master - packer-example echo vars

--- packer-demo/build-and-launch.sh
+++ packer-demo/build-and-launch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 ARTIFACT=`packer build -machine-readable packer-example.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
-echo $ARTIFACT
 echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
-echo $AMI_ID
 terraform init
 terraform apply
