9c4839124e9e98bfbf62d5dfbdd46048a4ff5ace
joe-avalos <jose.b.avalos@gmail.com>
2020-10-22 12:40:52 -0500

    master - docker-demo-3 region vars fix

--- docker-demo-3/vars.tf
+++ docker-demo-3/vars.tf
@@ -17,9 +17,9 @@ variable "ECS_INSTANCE_TYPE" {
 variable "ECS_AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-1924770e"
-    us-west-2 = "ami-56ed4936"
-    eu-west-1 = "ami-c8337dbb"
+    us-east-1 = "ami-0739f8cdb239fe9ae"
+    us-west-2 = "ami-08d0eee5e00da8a9b"
+    eu-west-1 = "ami-09b9e380df60300c8"
   }
 }
 
